ad99c3634abf9aac62630c50c200342ac1f5006f
Nicolas Léveillé <nicolas@uucidl.com>
2014-03-16 13:44:11 +0100

    fix application bundle build script on Mavericks
    Ruby 1.9 >= don't like ftools and moved this functionality to fileutils.

--- lib/scripts/IOS/make-application-bundle.rb
+++ lib/scripts/IOS/make-application-bundle.rb
@@ -3,43 +3,42 @@
 # This script is used to build an IOS application bundle from a plain executable.
 #
 
-require 'ftools'
 require 'fileutils'
 
 INFO_PLIST = %q{<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-	<key>CFBundleDevelopmentRegion</key>
-	<string>English</string>
-	<key>CFBundleExecutable</key>
-	<string>#{@executable}</string>
-	<key>CFBundleGetInfoString</key>
-	<string>#{@info}</string>
-	<key>CFBundleIconFile</key>
-	<string>#{@icon}</string>
-	<key>CFBundleIdentifier</key>
-	<string>#{@identifier}</string>
-	<key>CFBundleInfoDictionaryVersion</key>
-	<string>6.0</string>
-	<key>CFBundleDisplayName</key>
-	<string>#{@name}</string>
-	<key>CFBundleName</key>
-	<string>#{@name}</string>
-	<key>CFBundlePackageType</key>
-	<string>APPL</string>
-	<key>CFBundleSignature</key>
-	<string>????</string>
-	<key>CFBundleShortVersionString</key>
-	<string>#{@short_info}</string>
-	<key>CFBundleSignature</key>
-	<string>#{@signature}</string>
-	<key>CFBundleVersion</key>
-	<string>#{@version}</string>
-	<key>NSMainNibFile</key>
-	<string>#{@nib_file}</string>
-	<key>LSRequiresIPhoneOS</key>
-	<true/>
+        <key>CFBundleDevelopmentRegion</key>
+        <string>English</string>
+        <key>CFBundleExecutable</key>
+        <string>#{@executable}</string>
+        <key>CFBundleGetInfoString</key>
+        <string>#{@info}</string>
+        <key>CFBundleIconFile</key>
+        <string>#{@icon}</string>
+        <key>CFBundleIdentifier</key>
+        <string>#{@identifier}</string>
+        <key>CFBundleInfoDictionaryVersion</key>
+        <string>6.0</string>
+        <key>CFBundleDisplayName</key>
+        <string>#{@name}</string>
+        <key>CFBundleName</key>
+        <string>#{@name}</string>
+        <key>CFBundlePackageType</key>
+        <string>APPL</string>
+        <key>CFBundleSignature</key>
+        <string>????</string>
+        <key>CFBundleShortVersionString</key>
+        <string>#{@short_info}</string>
+        <key>CFBundleSignature</key>
+        <string>#{@signature}</string>
+        <key>CFBundleVersion</key>
+        <string>#{@version}</string>
+        <key>NSMainNibFile</key>
+        <string>#{@nib_file}</string>
+        <key>LSRequiresIPhoneOS</key>
+        <true/>
 </dict>
 </plist>}
 
@@ -72,7 +71,7 @@ PACKAGE_NAME = ARGV[0]
 EXECUTABLE_PATH = File.expand_path(ARGV[1])
 DEST = File.expand_path("#{File.dirname($0)}/../../OUTPUT")
 
-def die(msg) 
+def die(msg)
   print "#{msg}\n"
   exit 1
 end
@@ -81,7 +80,7 @@ if File.exists? EXECUTABLE_PATH
   @executable = File.basename(EXECUTABLE_PATH)
   BUNDLE_NAME = File.basename(EXECUTABLE_PATH, '.elf')
   BUNDLE_PATH = File.dirname(EXECUTABLE_PATH) + "/" + BUNDLE_NAME + ".app"
-  
+
   @name = BUNDLE_NAME
 
   @executable_path = "#{BUNDLE_PATH}/#{@executable}";
@@ -90,30 +89,30 @@ if File.exists? EXECUTABLE_PATH
     FileUtils.remove_dir("#{BUNDLE_PATH}", true)
   end
 
-  File.makedirs("#{BUNDLE_PATH}")
-  File.install(EXECUTABLE_PATH, @executable_path, 0755)
+  FileUtils.makedirs("#{BUNDLE_PATH}")
+  FileUtils.install(EXECUTABLE_PATH, @executable_path, 0755)
   (2 ... ARGV.length).each {|i|
     file = ARGV[i]
 
     if File.directory? file or File.exists? file then
-	dest = "#{BUNDLE_PATH}/"
+        dest = "#{BUNDLE_PATH}/"
 
       FileUtils.cp_r(file, dest)
       if /\.icns$/ =~ file
-	@icon = File.basename(file)
+        @icon = File.basename(file)
       end
     else
       print "Could not find #{file}\n"
     end
   }
-  
+
   @identifier = PACKAGE_NAME.gsub('/', '.') + "." + BUNDLE_NAME
   @bundle_name = BUNDLE_NAME
   @signature = BUNDLE_NAME
-  
+
   info_plist = File.open("#{BUNDLE_PATH}/Info.plist", "w")
   info_plist.write(produce(INFO_PLIST))
-  info_plist.close   
+  info_plist.close
 else
   raise "File not found: #{EXECUTABLE_PATH}"
 end
--- lib/scripts/MACOSX/make-application-bundle.rb
+++ lib/scripts/MACOSX/make-application-bundle.rb
@@ -3,41 +3,40 @@
 # This script is used to build an OSX application bundle from a plain executable.
 #
 
-require 'ftools'
 require 'fileutils'
 
 INFO_PLIST = %q{<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-	<key>CFBundleDevelopmentRegion</key>
-	<string>English</string>
-	<key>CFBundleExecutable</key>
-	<string>#{@executable}</string>
-	<key>CFBundleGetInfoString</key>
-	<string>#{@info}</string>
-	<key>CFBundleIconFile</key>
-	<string>#{@icon}</string>
-	<key>CFBundleIdentifier</key>
-	<string>#{@identifier}</string>
-	<key>CFBundleInfoDictionaryVersion</key>
-	<string>6.0</string>
-	<key>CFBundleName</key>
-	<string>#{@name}</string>
-	<key>CFBundlePackageType</key>
-	<string>APPL</string>
-	<key>CFBundleShortVersionString</key>
-	<string>#{@short_info}</string>
-	<key>CFBundleSignature</key>
-	<string>#{@signature}</string>
-	<key>CFBundleVersion</key>
-	<string>#{@version}</string>
-	<key>NSHumanReadableCopyright</key>
-	<string>#{@copyright_info}</string>
-	<key>NSMainNibFile</key>
-	<string>#{@nib_file}</string>
-	<key>NSPrincipalClass</key>
-	<string>NSApplication</string>
+        <key>CFBundleDevelopmentRegion</key>
+        <string>English</string>
+        <key>CFBundleExecutable</key>
+        <string>#{@executable}</string>
+        <key>CFBundleGetInfoString</key>
+        <string>#{@info}</string>
+        <key>CFBundleIconFile</key>
+        <string>#{@icon}</string>
+        <key>CFBundleIdentifier</key>
+        <string>#{@identifier}</string>
+        <key>CFBundleInfoDictionaryVersion</key>
+        <string>6.0</string>
+        <key>CFBundleName</key>
+        <string>#{@name}</string>
+        <key>CFBundlePackageType</key>
+        <string>APPL</string>
+        <key>CFBundleShortVersionString</key>
+        <string>#{@short_info}</string>
+        <key>CFBundleSignature</key>
+        <string>#{@signature}</string>
+        <key>CFBundleVersion</key>
+        <string>#{@version}</string>
+        <key>NSHumanReadableCopyright</key>
+        <string>#{@copyright_info}</string>
+        <key>NSMainNibFile</key>
+        <string>#{@nib_file}</string>
+        <key>NSPrincipalClass</key>
+        <string>NSApplication</string>
 </dict>
 </plist>}
 
@@ -66,7 +65,7 @@ end
 PACKAGE_NAME = ARGV[0]
 EXECUTABLE_PATH = File.expand_path(ARGV[1])
 
-def die(msg) 
+def die(msg)
   print "#{msg}\n"
   exit 1
 end
@@ -75,45 +74,45 @@ if File.exists? EXECUTABLE_PATH
   @executable = File.basename(EXECUTABLE_PATH)
   BUNDLE_NAME = File.basename(EXECUTABLE_PATH, '.elf')
   BUNDLE_PATH = File.dirname(EXECUTABLE_PATH) + "/" + BUNDLE_NAME + ".app"
-  
+
   @executable_path = "#{BUNDLE_PATH}/Contents/MacOS/#{@executable}";
 
   if File.directory? BUNDLE_PATH then
     FileUtils.remove_dir("#{BUNDLE_PATH}", true)
   end
 
-  File.makedirs("#{BUNDLE_PATH}/Contents/MacOS/")
-  File.install(EXECUTABLE_PATH, @executable_path, 0755)
-  File.makedirs("#{BUNDLE_PATH}/Contents/Resources/")
-  File.makedirs("#{BUNDLE_PATH}/Contents/Libraries/")
+  FileUtils.makedirs("#{BUNDLE_PATH}/Contents/MacOS/")
+  FileUtils.install(EXECUTABLE_PATH, @executable_path, :mode => 0755)
+  FileUtils.makedirs("#{BUNDLE_PATH}/Contents/Resources/")
+  FileUtils.makedirs("#{BUNDLE_PATH}/Contents/Libraries/")
   (2 ... ARGV.length).each {|i|
     file = ARGV[i]
 
     if File.directory? file or File.exists? file then
       if file =~ /\.dylib$/ then
-	dest = "#{BUNDLE_PATH}/Contents/Libraries/"
-	libname = File.basename(file)
-	system ("install_name_tool -change #{file} @executable_path/../Libraries/#{libname} #{@executable_path}");
+        dest = "#{BUNDLE_PATH}/Contents/Libraries/"
+        libname = File.basename(file)
+        system ("install_name_tool -change #{file} @executable_path/../Libraries/#{libname} #{@executable_path}");
       else
-	dest = "#{BUNDLE_PATH}/Contents/Resources/"
+        dest = "#{BUNDLE_PATH}/Contents/Resources/"
       end
 
       FileUtils.cp_r(file, dest)
       if /\.icns$/ =~ file
-	@icon = File.basename(file)
+        @icon = File.basename(file)
       end
     else
       print "Could not find #{file}\n"
     end
   }
-  
+
   @identifier = PACKAGE_NAME.gsub('/', '.') + "." + BUNDLE_NAME
   @bundle_name = BUNDLE_NAME
   @signature = BUNDLE_NAME
-  
-  info_plist = File.open("#{BUNDLE_PATH}/Contents/Info.plist", "w")	
+
+  info_plist = File.open("#{BUNDLE_PATH}/Contents/Info.plist", "w")
   info_plist.write(produce(INFO_PLIST))
-  info_plist.close   
+  info_plist.close
 else
   raise "File not found: #{EXECUTABLE_PATH}"
 end
